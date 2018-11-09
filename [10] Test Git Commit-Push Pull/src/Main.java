0906c3c79a1f8c31b6b66b35e83f7ffd6ff151a0
ament <ament@LAPTOP-6FMM6BQ9.station>
2018-11-10 00:36:31 +0100

    Aggiunta di foreach funzionante

--- [10] Test Git Commit-Push Pull/src/Main.java
+++ [10] Test Git Commit-Push Pull/src/Main.java
@@ -5,6 +5,16 @@ public class Main {
 		// TODO Auto-generated method stub   
 		System.out.println("1) ");
 		System.err.println("Change occurred. ");
+		
+		int  [] arr = {1,2,3,4,5,6};
+        
+        for(int i = 0; i<arr.length; i++ ) {
+            System.out.println("Arr["+i+"] -> " + arr[i]);
+        }
+        
+        for (int temp : arr) {
+            System.out.println("Arr[foreach] -> " + temp);
+        }
 
 	}
 
